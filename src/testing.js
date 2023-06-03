fa7c1853c7341158957304da3dc680ced2eba21e
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-03 16:05:41 -0500

    update

--- src/testing.js
+++ src/testing.js
@@ -1,18 +1,26 @@
  const correctAnswer = "closure"; // Correct answer to your JavaScript problem
 
-        function checkAnswer() {
-            const userAnswer = document.getElementById('userAnswer').value;
-            if (userAnswer.trim().toLowerCase() === correctAnswer.toLowerCase()) {
-                document.getElementById('jsChallenge').style.display = 'none';
-                document.getElementById('siteContent').style.display = 'block';
-            } else {
-                alert('Wrong answer, try again.');
-            }
-        }
+if (localStorage.getItem('solved')) {
+    document.getElementById('jsChallenge').style.display = 'none';
+    document.getElementById('siteContent').style.display = 'block';
+}
+
+function checkAnswer() {
+  const userAnswer = document.getElementById('userAnswer').value;
+
+  if (userAnswer.trim().toLowerCase() === correctAnswer.toLowerCase()) {
+    // Якщо відповідь вірна, зберігаємо це в localStorage
+    localStorage.setItem('solved', 'true');
+    document.getElementById('jsChallenge').style.display = 'none';
+    document.getElementById('siteContent').style.display = 'block';
+  } else {
+    alert('Wrong answer, try again.');
+  }
+}
 
 document.body.style.backgroundImage = "url('https://wallpaperaccess.com/full/3347901.jpg')";
-const aElements = document.getElementsByTagName("a"); // Отримуємо всі елементи a
-const aArray = Array.from(aElements); // Перетворюємо HTMLCollection в масив
+ aElements = document.getElementsByTagName("a"); // Отримуємо всі елементи a
+ aArray = Array.from(aElements); // Перетворюємо HTMLCollection в масив
 
 aArray.forEach(a => {
   a.style.listStyle = "none"; // Видаляємо маркери
@@ -22,15 +30,15 @@ aArray.forEach(a => {
 });
 
 // Щоб розмістити елементи a по центру, ви можете стилізувати їх контейнер (наприклад, ul)
-const ulElement = document.getElementsByTagName("ul")[0]; // Припустимо, що у вас є тільки один елемент ul
+ ulElement = document.getElementsByTagName("ul")[0]; // Припустимо, що у вас є тільки один елемент ul
 
 ulElement.style.display = "flex"; // Встановлюємо тип відображення на flex для центрування
 ulElement.style.justifyContent = "center"; // Центруємо елементи по горизонталі
 ulElement.style.alignItems = "center"; // Центруємо елементи по вертикалі
 
 // Центруємо h1 і змінюємо колір на оранжевий
-const h1Elements = document.getElementsByTagName("h1"); // Отримуємо всі елементи h1
-const h1Array = Array.from(h1Elements); // Перетворюємо HTMLCollection в масив
+ h1Elements = document.getElementsByTagName("h1"); // Отримуємо всі елементи h1
+ h1Array = Array.from(h1Elements); // Перетворюємо HTMLCollection в масив
 
 h1Array.forEach(h1 => {
   h1.style.color = "yellow"; // Змінюємо колір на оранжевий
