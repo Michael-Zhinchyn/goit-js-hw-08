946ddecfdd7793bc8232f91b811f5070dbf60748
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-03 12:33:26 -0500

    update task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player  from "@vimeo/player"
+import throttle from "lodash.throttle"
+
+
+// отримую попередньо встановлений елемент плеєра за його id
+const video = document.getElementById("vimeo-player")
+// ініціалізую Vimeo Player з цим елементом
+const player = new Player(video)
+
+
+
+// ініціалізую throttle function
+const throttledTimeUpdate = throttle(seconds => {
+
+    // Зберігаю час в локальне сховище.
+    localStorage.setItem('videoplayer-current-time', seconds);
+}, 1000);  // Throttle 1 раз в секунду
+
+// за допомогою методу on() відстежую подію timeupdate
+player.on('timeupdate', function (data) {
+    throttledTimeUpdate(data.seconds);
+});
+
+// якщо час відтворення збережено в локальне сховище, встановлюю час з сховища 
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+    player.setCurrentTime(savedTime);
+}
+
 
