dbedbb6fe1a29d5be812e88ed289729e7226666e
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-16 10:09:40 -0500

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,37 +1,39 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-const list = document.querySelector('.gallery')
+const list = document.querySelector('.gallery');
 
 function markup(items) {
-    return items.map((item) => `<li class="gallery__item">
+  return items
+    .map(
+      item => `<li class="gallery__item">
     <a class="gallery__link" href="${item.original}">
        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
     </a>
- </li>`).join("")
+ </li>`
+    )
+    .join('');
 }
 
-list.insertAdjacentHTML('beforeend', markup(galleryItems))
+list.insertAdjacentHTML('beforeend', markup(galleryItems));
 
 new SimpleLightbox('.gallery a', {
-    captionDelay: 250,
-    captionsData: 'alt',
-    captionPosition: 'bottom'
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
 });
 
-list.style.listStyle = 'none'
+list.style.listStyle = 'none';
 
-const pageLink = document.querySelector('a')
-pageLink.style.fontWeight = '600'
-pageLink.style.color = 'blue'
-pageLink.style.textDecoration = 'none'
-pageLink.style.display= 'flex'
-pageLink.style.justifyContent = 'end'
-pageLink.style.paddingRight = '20px'
-pageLink.href = '02-video.html'
-pageLink.textContent = 'Go To The Next Task - VIDEO'
-
-
+const pageLink = document.querySelector('a');
+pageLink.style.fontWeight = '600';
+pageLink.style.color = 'blue';
+pageLink.style.textDecoration = 'none';
+pageLink.style.display = 'flex';
+pageLink.style.justifyContent = 'end';
+pageLink.style.paddingRight = '20px';
+pageLink.href = '02-video.html';
+pageLink.textContent = 'Go To The Next Task - VIDEO';
