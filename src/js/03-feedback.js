396a06cfa2aabe04753bf8faaadd050be4e29708
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-24 12:35:05 -0500

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,71 +1,93 @@
-import throttle from "lodash.throttle"
+import throttle from 'lodash.throttle';
 
+// Вибір елементів DOM
 const form = document.querySelector('.feedback-form');
 const emailInput = form.elements.email;
 const messageInput = form.elements.message;
 const submitButton = document.querySelector('button');
 
+// Отримання збереженого фідбеку з localStorage
 const savedFeedback = JSON.parse(localStorage.getItem('feedback-form-state'));
 if (savedFeedback) {
-    emailInput.value = savedFeedback.email;
-    messageInput.value = savedFeedback.message;
-    submitButton.disabled = !(emailInput.value && messageInput.value);
+  // Відновлення значень введення збереженого фідбеку
+  emailInput.value = savedFeedback.email;
+  messageInput.value = savedFeedback.message;
+  // Вимкнення кнопки відправки, якщо електронна пошта або повідомлення порожні
+  submitButton.disabled = !(emailInput.value && messageInput.value);
 }
 
-form.addEventListener('input', throttle(() => {
-    localStorage.setItem('feedback-form-state', JSON.stringify({
+// Обмежений (throttled) обробник подій для змін вводу
+form.addEventListener(
+  'input',
+  throttle(() => {
+    // Збереження поточних значень введення у localStorage
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify({
         email: emailInput.value,
-        message: messageInput.value
-    }));
+        message: messageInput.value,
+      })
+    );
 
+    // Активація або вимкнення кнопки відправки на основі значень введення
     submitButton.disabled = !(emailInput.value && messageInput.value);
-}, 500));
+  }, 500)
+);
 
+// Обробник подій для надсилання форми
 form.addEventListener('submit', event => {
-    event.preventDefault();
-    
-    console.log({
-        email: emailInput.value,
-        message: messageInput.value
-    });
-    
-    form.reset();
-    localStorage.removeItem('feedback-form-state');
-    submitButton.disabled = true;
+  event.preventDefault();
+
+  // Виведення електронної пошти та повідомлення у консоль
+  console.log({
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+
+  // Скидання форми та видалення збереженого фідбеку з localStorage
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+  submitButton.disabled = true;
 });
 
-const pageLink = document.querySelector('a')
+// Стилізація посилання сторінки для виходу
+const pageLink = document.querySelector('a');
 Object.assign(pageLink.style, {
-    fontWeight : '600',
-    display : 'flex',
-    justifyContent : 'end',
-    color : 'salmon',
-    textDecoration : 'none',
-    position : 'fixed',
-    right : '5%',
-    top : '10px'
-})
+  fontWeight: '600',
+  display: 'flex',
+  justifyContent: 'end',
+  color: 'salmon',
+  textDecoration: 'none',
+  position: 'fixed',
+  right: '5%',
+  top: '10px',
+});
 
-pageLink.textContent = 'CLICK HERE FOR EXIT'
-pageLink.href = 'https://dorneawhale.files.wordpress.com/2015/02/thank-you2.gif'
-form.style.margin= 'auto';
+// Встановлення вмісту та href для посилання сторінки
+pageLink.textContent = 'НАТИСНІТЬ ТУТ ДЛЯ ВИХОДУ';
+pageLink.href =
+  'https://dorneawhale.files.wordpress.com/2015/02/thank-you2.gif';
 
+// Стилізація елемента форми
+form.style.margin = 'auto';
+
+// Створення та стилізація елемента з іконкою
 const iconElement = document.createElement('div');
 iconElement.innerHTML = `
 <a href="02-video.html" style="text-decoration: none; color: inherit;">
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 50 50" style="vertical-align: middle;">
         <polyline points="35,10 15,25 35,40" stroke="#000000" stroke-width="4"/>
     </svg>
-    <span style="margin-left: 5px;">Back</span>
+    <span style="margin-left: 5px;">Назад</span>
 </a>`;
 document.body.prepend(iconElement);
 
-const icon = document.querySelector('svg')
-icon.style.fill = 'orange'
+// Стилізація SVG іконки
+const icon = document.querySelector('svg');
+icon.style.fill = 'orange';
 
-const span = document.querySelector('span')
-span.style.color = 'orange'
-span.style.fontSize = '18px'
-span.style.fontWeight = '500'
-
-
+// Стилізація тексту іконки
+const span = document.querySelector('span');
+span.style.color = 'orange';
+span.style.fontSize = '18px';
+span.style.fontWeight = '500';
