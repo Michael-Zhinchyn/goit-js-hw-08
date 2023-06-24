b2e505c106655ee5faaae4c21d13d5119c928a70
Michael-Zhinchyn <124111477+Michael-Zhinchyn@users.noreply.github.com>
2023-06-24 12:30:40 -0500

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,46 +1,46 @@
-import Player  from "@vimeo/player"
-import throttle from "lodash.throttle"
+// Імпортуємо Player з @vimeo/player, це модуль для роботи з Vimeo Player API
+import Player from '@vimeo/player';
 
+// Імпортуємо throttle з lodash, це функція, яка обмежує частоту виконання іншої функції
+import throttle from 'lodash.throttle';
 
-// отримую попередньо встановлений елемент плеєра за його id
-const video = document.getElementById("vimeo-player")
-// ініціалізую Vimeo Player з цим елементом
-const player = new Player(video)
+// Отримуємо елемент плеєра з DOM за його id
+const video = document.getElementById('vimeo-player');
+// Ініціалізуємо Vimeo Player з цим елементом
+const player = new Player(video);
 
-
-// ініціалізую throttle function
+// Ініціалізуємо throttle function, яка зберігає час відтворення в локальне сховище кожну секунду
 const throttledTimeUpdate = throttle(seconds => {
-    // Зберігаю час в локальне сховище.
-    localStorage.setItem('videoplayer-current-time', seconds);
-}, 1000);  // Throttle 1 раз в секунду
+  localStorage.setItem('videoplayer-current-time', seconds);
+}, 1000);
 
-// за допомогою методу on() відстежую подію timeupdate
+// Відстежуємо подію timeupdate в плеєрі і запускаємо throttledTimeUpdate з часом відтворення як аргумент
 player.on('timeupdate', function (data) {
-    throttledTimeUpdate(data.seconds);
+  throttledTimeUpdate(data.seconds);
 });
 
-// якщо час відтворення збережено в локальне сховище, встановлюю час з сховища 
+// Якщо в локальному сховищі є збережений час відтворення, встановлюємо його в плеєр
 const savedTime = localStorage.getItem('videoplayer-current-time');
-
 if (savedTime) {
-    player.setCurrentTime(savedTime);
+  player.setCurrentTime(savedTime);
 }
 
-
-const pageLink = document.querySelector('a')
-pageLink.style.fontWeight = '600'
-pageLink.style.color = 'blue'
-pageLink.style.textDecoration = 'none'
-// pageLink.style.display= 'flex'
-// pageLink.style.justifyContent = 'end'
-pageLink.style.paddingRight = '20px'
-pageLink.textContent = 'Go To Next Task - Feedback'
+// Задаємо стилі і текст для посилання на наступне завдання
+const pageLink = document.querySelector('a');
+pageLink.style.fontWeight = '600';
+pageLink.style.color = 'blue';
+pageLink.style.textDecoration = 'none';
+pageLink.style.paddingRight = '20px';
+pageLink.textContent = 'Go To Next Task - Feedback';
 pageLink.href = '03-feedback.html';
+
+// Задаємо стилі для елемента відео
 video.style.position = 'absolute';
 video.style.top = '35%';
 video.style.left = '50%';
 video.style.transform = 'translate(-50%, -50%)';
 
+// Створюємо новий елемент div та добавляємо йому HTML
 const iconElement = document.createElement('div');
 iconElement.innerHTML = `
 <a href="01-gallery.html" style="text-decoration: none; color: inherit;">
@@ -50,29 +50,21 @@ iconElement.innerHTML = `
     <span style="margin-left: 5px;">Back</span>
 </a>`;
 
+// Добавляємо створене посилання до нашого div елемента та вставляємо його в DOM
 iconElement.appendChild(pageLink);
-document.body.prepend(iconElement)
+document.body.prepend(iconElement);
 
+// Задаємо стилі для div елемента
+iconElement.style.display = 'flex';
+iconElement.style.justifyContent = 'space-between';
+iconElement.style.alignItems = 'center';
 
-iconElement.style.display = 'flex'
-iconElement.style.justifyContent = 'space-between'
-iconElement.style.alignItems = 'center'
+// Задаємо стилі для svg іконки
+const icon = document.querySelector('svg');
+icon.style.fill = 'orange';
 
-const icon = document.querySelector('svg')
-icon.style.fill = 'orange'
-
-const span = document.querySelector('span')
-span.style.color = 'orange'
-span.style.fontSize = '18px'
-span.style.fontWeight = '500'
-
-
-
-
-
-
-
-
-
-
-
+// Задаємо стилі для тексту
+const span = document.querySelector('span');
+span.style.color = 'orange';
+span.style.fontSize = '18px';
+span.style.fontWeight = '500';
